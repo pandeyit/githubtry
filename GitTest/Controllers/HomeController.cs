4dc01c3a026026bdd51b0337a93a13a9b30d87c0
SCARABEE_NT\js <john.selen@scarabee.com>
2018-07-16 12:38:21 +0200

    worked on flow of labelling kiosk

--- GitTest/Controllers/HomeController.cs
+++ GitTest/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace GitTest.Controllers
         public ActionResult About()
         {
 
-            ViewBag.Message = "Your avvv develfffffope application description page.";
+            ViewBag.Message = "Feature branch for labelling kiosk";
 
             return View();
         }
